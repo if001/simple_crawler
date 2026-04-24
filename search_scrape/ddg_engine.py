e6eed88cbe834cc26d0d3c59b261f86b18445279
if001 <otomijuf.004@gmail.com>
2026-04-24 20:59:31 +0900

    fix header

--- search_scrape/ddg_engine.py
+++ search_scrape/ddg_engine.py
@@ -34,12 +34,14 @@ class DuckDuckGoHtmlSearchEngine(SearchEngine):
             "https://html.duckduckgo.com/html/",
             params=params,
             headers={
-                "User-Agent": "Mozilla/5.0 (Windows NT 11.0; Win64; x64) AppleWebKit/605.1.15 (KHTML, like Gecko) Chrome/147.0.7727.56",
-                "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/147.0.0.0 Safari/537.36",
+                "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
                 "Accept-Language": "ja,en-US;q=0.9,en;q=0.8",
-                "Accept-Encoding": "gzip, deflate, br",
+                "Accept-Encoding": "gzip, deflate, br, zstd",
+                "Content-Type": "application/x-www-form-urlencoded",
                 "Connection": "keep-alive",
                 "Upgrade-Insecure-Requests": "1",
+                "Referer": "https://html.duckduckgo.com/",
             },
             timeout=15.0,
         )
